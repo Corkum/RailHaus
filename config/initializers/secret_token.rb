9b3f9802d04f8a25e272fac856ddb1b7b712e437
Peter Dietz <dietz.72@osu.edu>
2011-03-14 22:58:35 -0400

    Initial creation of rails cloudhaus site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haus::Application.config.secret_token = 'af3bb26065621e3c1d6f6893bbd230d48ce3cf06ea233b11401bd609a378a15243b17eeef84d59f004bd34850306a689f231d2bc26d2e974b40e3fe3eca4c103'

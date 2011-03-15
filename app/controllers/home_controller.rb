2286b27aa6de3aa94acf3074d3003c43c1db9fc3
Peter Dietz <dietz.72@osu.edu>
2011-03-15 01:15:51 -0400

    Events page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @event = Event.find(1)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @event }
+    end
   end
 
 end

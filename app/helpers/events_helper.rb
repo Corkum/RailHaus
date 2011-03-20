4dbab442a470c39b7d866b41c3f3f2af04692bd8
Peter Dietz <dietz.72@osu.edu>
2011-03-20 17:02:26 -0400

    Created event_time_ago helper and added to events index

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+  def event_time_ago(date)
+    if date < Date.today()
+      "was " + time_ago_in_words(date, false) + " ago"
+    elsif date > Date.today()
+      "in " + time_ago_in_words(date, false)
+    else
+      "Today"
+    end
+  end
+  
 end
